085fd5a1860d549779aac3792ea6aa5a9f622276
lindanhh <57466933+lindanhh@users.noreply.github.com>
2020-05-27 19:18:22 -0700

    Add files via upload

---
+++ big/includes/footer.php
@@ -0,0 +1,10 @@
+
+<!-- START Footer -->
+<footer>
+  <p><small>&copy; 2016 - 2020 by <a href="#" target="_blank">Sara Newman</a>, All Rights Reserved ~ <a href="http://validator.w3.org/check/referer" target="_blank">Valid HTML</a> ~ <a href="http://jigsaw.w3.org/css-validator/check?uri=referer" target="_blank">Valid CSS</a></small></p>
+</footer>
+<!-- END Footer --> 
+</div>
+<!-- END Wrapper -->
+</body>
+</html>
---
+++ big/includes/header.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+<title>BIG Final Project for Web120 by Linda Danh</title>
+    <meta name="viewport" content="width=device-width" />
+    <meta name="robots" content="noindex,nofollow" />
+    <meta charset="utf-8">
+    <link rel="stylesheet" href="css/nav.css" />
+    <link rel="stylesheet" href="css/big.css" />
+    <link rel="stylesheet" href="css/form.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" />
+    <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script src="https://s3.amazonaws.com/menumaker/menumaker.min.js" type="text/javascript"></script>
+    <script src="js/script.js"></script>
+</head>
+
+<body>
+<!-- START Wrapper -->
+<div class="wrapper">
+<header>
+  <h1><a href="index.php"><i class="logo fa fa-home"></i>Linda Danh's Web 120 Final Project</a></h1>
+  <nav id="cssmenu">
+  <ul>
+     <li><a href="index.php"><span><i class="fa fa-fw fa-bank"></i> WEB120 Portal</span></a></li>
+     <li><a href="index.php"><span><i class="fa fa-fw fa-home"></i> Home</span></a></li>
+     <li><a href="flexbox.php"><span>Flexbox</span></a></li>
+     <li><a href="galleries.php"><span>Galleries</span></a></li>
+     <li><a href="shoppingcarts.php"><span>Shopping Carts</span></a></li>
+     <li><a href="siteapp.php"><span>Site vs App</span></a></li>
+     <li><a href="#"><span><i class="fa fa-fw fa-chevron-down"></i> Google</span></a>
+        <ul>
+           <li><a href="map.php"><span>Calendar</span></a></li>
+           <li><a href="calendar.php"><span>Form</span></a></li>
+           <li><a href="youtube.php"><span>Map</span></a></li>
+        </ul>
+     </li>
+     <li><a href="parallax.php"><span>Youtube</span></a></li>
+     <li><a href="webcam.php"><span>Web Cam</span></a></li>
+  </ul>
+</nav>
+</header>
+        
+<!-- START LEFT COL -->
+<section>
+ <h2 class="pageID">Linda Danh Linda Danh</h2>    
